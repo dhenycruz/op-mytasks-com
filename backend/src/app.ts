df69ba678e0ba4d4e08fb2377f18a66ed2901e59
Dheniarley Cruz <dheniarley.ds@gamil.com>
2022-05-19 13:28:16 -0300

    aplicaded POO app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+class App {
+  public express: express.Application;
+
+  constructor () {
+    this.express = express();
+  }
+}
+
+export default App;
--- backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-
-const app = express();
-
-const PORT = process.env.PORT || 3001;
-
-app.get('/', (_req: Request, res: Response) => res.send('Home'));
-
-app.listen( console.log(`Escutando na porta ${PORT}`));
